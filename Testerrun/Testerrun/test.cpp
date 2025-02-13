b0cedc408795256a1359e203ed6b37618a030667
ILikePotatoes127 <HuDaniel79@gmail.com>
2025-02-12 21:48:39 -0800

    day 3

--- Testerrun/Testerrun/test.cpp
+++ Testerrun/Testerrun/test.cpp
@@ -15,7 +15,8 @@ int CALLBACK WinMain(
     LPSTR lpCmdLine,
     int nCmdShow)
 {
-
+    
+    /*
     char SmallS; //8 bits 256 values [-128, 127]
     char unsigned SmallU; // 8 bits unsigned - 256 different values [0,255]
 
@@ -24,20 +25,15 @@ int CALLBACK WinMain(
 
     int LargeS; // 32 bits - 4 billion
     int unsigned LargeU;
+    */
 
     char unsigned Test; 
+    char unsigned* TestPointer;
+
+    TestPointer = &Test;
 
     Test = 255;
-
     Test = Test + 1;
-
-
-    int Integer;
-
-    Integer = 5;
-    Integer = 5 + 2;
-    Integer = Integer + 5;
-
 
 
     foo();
